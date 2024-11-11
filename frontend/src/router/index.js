4cb17f5e356ebfdfbf90c670ffde9ea21b587b62
Mriris <2647381485@qq.com>
2024-11-11 21:48:03 +0800

    bootstrap模板vue化

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';  // 导入 Home 组件
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 
 const routes = [
+    { path: '/', name: 'Home', component: Home },  // 默认路径指向 Home 组件
     { path: '/login', component: Login },
     { path: '/register', component: Register }
 ];
