0a7fab258cb968a0028da172dd82d343b1283d41
Mriris <2647381485@qq.com>
2024-11-11 19:48:54 +0800

    添加bootstrap框架

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <Header />   <!-- 导航栏组件 -->
+    <router-view />  <!-- 用于显示页面内容 -->
+    <Footer />   <!-- 页脚组件 -->
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 };
 </script>
 
 <style>
-/* 样式 */
+/* 在这里添加全局样式 */
 </style>
